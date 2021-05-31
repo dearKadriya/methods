dda1c20432879221aa6e11cd22df290321cd7ca4
artemkosnikov <Boxberry123>
2021-05-31 22:35:32 +0300

    init commit

---
+++ src/Character.js
@@ -0,0 +1,81 @@
+export class Character {
+  constructor(name) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Имя должно содержать от двух до десяти символов');
+    }
+    this.name = name;
+    this.health = 100;
+    this.level = 1;
+  }
+
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error('Нельзя повысить левел умершего');
+    }
+    this.level += 1;
+    this.attack += (this.attack * 20) / 100;
+    this.defence += (this.defence * 20) / 100;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health <= 0) {
+      throw new Error('Мертвый персонаж не получит урон.');
+    }
+    this.health -= points * (1 - this.defence / 100);
+  }
+}
+
+export class Bowerman extends Character {
+  constructor(name) {
+    super(name);
+    this.type = 'Bowerman';
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Swordsman extends Character {
+  constructor(name) {
+    super(name);
+    this.type = 'Swordsman';
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+export class Magician extends Character {
+  constructor(name) {
+    super(name);
+    this.type = 'Magician';
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export class Undead extends Character {
+  constructor(name) {
+    super(name);
+    this.type = 'Undead';
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Zombie extends Character {
+  constructor(name) {
+    super(name);
+    this.type = 'Zombie';
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+export class Daemon extends Character {
+  constructor(name) {
+    super(name);
+    this.type = 'Daemon';
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
---
+++ src/character.test.js
@@ -0,0 +1,79 @@
+import {
+  Bowerman, Daemon, Swordsman, Magician, Undead, Zombie, Character,
+} from './Character';
+
+test('Проверка превышение длины имени', () => {
+  expect(() => { new Character('01234567891'); }).toThrowError('Имя должно содержать от двух до десяти символов');
+});
+
+test('Проверка слишком короткого имени', () => {
+  expect(() => { new Character('о'); }).toThrowError('Имя должно содержать от двух до десяти символов');
+});
+
+test('Проверка создания Bowerman', () => {
+  const result = new Bowerman('тест');
+  expect(result).toEqual({
+    attack: 25, defence: 25, health: 100, level: 1, name: 'тест', type: 'Bowerman',
+  });
+});
+
+test('Проверка создания Daemon', () => {
+  const result = new Daemon('тест');
+  expect(result).toEqual({
+    attack: 10, defence: 40, health: 100, level: 1, name: 'тест', type: 'Daemon',
+  });
+});
+
+test('Проверка создания Swordsman', () => {
+  const result = new Swordsman('тест');
+  expect(result).toEqual({
+    attack: 40, defence: 10, health: 100, level: 1, name: 'тест', type: 'Swordsman',
+  });
+});
+
+test('Проверка создания Magician', () => {
+  const result = new Magician('тест');
+  expect(result).toEqual({
+    attack: 10, defence: 40, health: 100, level: 1, name: 'тест', type: 'Magician',
+  });
+});
+
+test('Проверка создания Undead', () => {
+  const result = new Undead('тест');
+  expect(result).toEqual({
+    attack: 25, defence: 25, health: 100, level: 1, name: 'тест', type: 'Undead',
+  });
+});
+
+test('Проверка создания Zombie', () => {
+  const result = new Zombie('тест');
+  expect(result).toEqual({
+    attack: 40, defence: 10, health: 100, level: 1, name: 'тест', type: 'Zombie',
+  });
+});
+
+test('Проверка повышения уровню', () => {
+  const result = new Zombie('тест');
+  result.levelUp();
+  expect(result).toEqual({
+    attack: 48, defence: 12, health: 100, level: 2, name: 'тест', type: 'Zombie',
+  });
+});
+
+test('Проверка, что уровень не повысится когда здоровье равно нулю', () => {
+  const result = new Zombie('тест');
+  result.health = 0;
+  expect(() => { result.levelUp(); }).toThrowError('Нельзя повысить левел умершего');
+});
+
+test('Проверка, что урон будет нанесен', () => {
+  const result = new Zombie('тест');
+  result.damage(10);
+  expect(result.health).toEqual(91);
+});
+
+test('Проверка, что урон не будет нанесен если здоровье ноль', () => {
+  const result = new Zombie('тест');
+  result.health = 0;
+  expect(() => { result.damage(10); }).toThrowError('Мертвый персонаж не получит урон.');
+});
